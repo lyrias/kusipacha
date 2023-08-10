fd12369cfed60b248bd9d1f2381e6957bb89c6e1
lyrias <java.lyria@gmail.com>
2023-08-10 11:50:51 +0200

    se agrego una IA

---
+++ js/cam.js
@@ -0,0 +1,39 @@
+const video = document.createElement('video');
+const canvas = document.getElementById('webcam-container');
+const context = canvas.getContext('2d');
+let currentStream;
+
+// Función para acceder a la cámara
+const startCamera = async (facingMode) => {
+    if (currentStream) {
+        currentStream.getTracks().forEach(track => track.stop());
+    }
+    
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: facingMode } });
+        currentStream = stream;
+        video.srcObject = stream;
+        video.play();
+    } catch (error) {
+        console.error('Error al acceder a la cámara:', error);
+    }
+};
+
+// Cambiar entre cámaras frontal y trasera
+//const toggleCamera = () => {
+//    const facingMode = (currentStream && currentStream.getVideoTracks()[0].getSettings().facingMode === 'user') ? 'environment' : 'user';
+//    startCamera(facingMode);
+//};
+
+// Iniciar con la cámara frontal
+startCamera('user');
+
+video.addEventListener('play', () => {
+    const updateCanvas = () => {
+        if (!video.paused && !video.ended) {
+            context.drawImage(video, 0, 0, canvas.width, canvas.height);
+            requestAnimationFrame(updateCanvas);
+        }
+    };
+    updateCanvas();
+});
